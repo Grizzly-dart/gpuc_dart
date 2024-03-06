549388ac47e98b255d382acd160bce41e4ec9edf
Ravi Teja Gudapati <tejainece@gmail.com>
2024-03-06 02:44:07 +0100

    minor changes

--- example/gpuc_dart_example.dart
+++ example/gpuc_dart_example.dart
@@ -2,11 +2,14 @@ import 'package:gpuc_dart/gpuc_dart.dart';
 import 'package:gpuc_dart/src/cuda.dart';
 
 void test() {
+  final watch = Stopwatch()..start();
   print(CudaFFIFunctions.getDeviceProps(0));
   final t1 = Tensor.random(Size.twoD(512, 512));
   final t2 = Tensor.random(Size.twoD(512, 512));
   final t3 = t1 + t2;
-  print(t3.toList());
+  t3.to(DeviceType.c);
+  print(watch.elapsed);
+  // print(t3.toList());
 }
 
 void main() async {
