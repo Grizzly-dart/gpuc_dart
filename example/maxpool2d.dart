0e1c81612e84d980f164fc77d4b9ce4c94131dbc
Ravi Teja Gudapati <tejainece@gmail.com>
2024-03-16 02:25:07 +0100

    minor changes

--- example/maxpool2d.dart
+++ example/maxpool2d.dart
@@ -17,14 +17,16 @@ Future<void> test(
   final input = Tensor.fromList(
       List.generate(16 * 16, (index) => rand.nextDouble()),
       size: Dim([1, 1, 16, 16]));
-  input.printTextTable();
+  // input.printTextTable();
   final maxPool2D = MaxPool2D(kernelSize, stride: stride, padding: padding);
   final out = await maxPool2D.forward(input);
   print(out.size);
-  out.printTextTable();
+  print(out.as1d);
+  // out.printTextTable();
   final tenson = TensonCmd();
   final out2 = await tenson.maxPool2D(
       kernelSize: kernelSize, input: input, stride: stride, padding: padding);
-  out2.printTextTable();
+  print(out2.as1d);
+  // out2.printTextTable();
   out.as1d.assertEqual(out2.as1d, eps: 1e-6);
 }
