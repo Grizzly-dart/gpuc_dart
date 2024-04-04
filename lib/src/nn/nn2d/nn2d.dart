8279de1a99c5a87150ca4bbdffed3dd879360f30
Ravi Teja Gudapati <tejainece@gmail.com>
2024-04-04 09:56:07 +0200

    normalization

--- lib/src/nn/nn2d/nn2d.dart
+++ lib/src/nn/nn2d/nn2d.dart
@@ -1,6 +1,6 @@
 import 'package:gpuc_dart/gpuc_dart.dart';
 
-export 'groupnorm2d.dart';
+export '../normalization/groupnorm2d.dart';
 export 'conv2d.dart';
 export 'maxpool2d.dart';
 
--- lib/src/nn/normalization/groupnorm.dart
+++ lib/src/nn/normalization/groupnorm.dart
@@ -2,21 +2,34 @@ import 'dart:async';
 
 import 'package:gpuc_dart/gpuc_dart.dart';
 
-class GroupNorm extends Layer2D<double> {
+class GroupNorm extends Layer<double> {
   final int numGroups;
   final int numChannels;
   double eps;
   final bool affine;
 
+  late final Tensor<double>? gamma;
+  late final Tensor<double>? beta;
+
   GroupNorm(this.numGroups, this.numChannels,
       {this.eps = 1e-5, this.affine = true}) {
-    // TODO initialize weights and biases
+    assert(numChannels % numGroups == 0);
+    if(affine) {
+      gamma = F64Tensor.generate(numChannels, (size, index) => 1);
+      beta = F64Tensor.generate(numChannels, (size, index) => 0);
+    } else {
+      gamma = null;
+      beta = null;
+    }
   }
 
   @override
   Future<Tensor<double>> compute(FutureOr<Tensor<double>> input,
       {covariant Tensor<double>? out, bool training = false}) async {
     final inp = await input;
+    assert(inp.size.dims > 2);
+    assert(inp.size[1] == numChannels);
+
     throw UnimplementedError();
   }
 
